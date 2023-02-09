4e001a4cc915e5143b6e5eb6a46d4d164d260b8d
nikPipe <123520318+nikPipe@users.noreply.github.com>
2023-02-09 00:14:15 -0800

    added some sample file

---
+++ widget/sample/mainWindow.py
@@ -0,0 +1,61 @@
+from import_module import *
+
+
+
+class SAMPLE_QMainWindow():
+    def widget_name(self):
+        '''
+
+        :return:
+        '''
+        print('widget is gong to create')
+
+
+    def menuBar(self, window, menuBarDic={}):
+        '''
+
+        :return:
+        '''
+        menubar = window.menuBar()
+        for each in menuBarDic:
+            menu = menubar.addMenu(each)
+
+
+        '''
+        filemenu = menubar.addMenu('File')
+        filemenu.addAction(QAction('something', window))
+        menubar.addMenu('Edit')
+        '''
+
+    def createAct(self, name, shortcut, toolTip, callback):
+        '''
+
+        :param name:
+        :param shortcut:
+        :param toolTip:
+        :param callback:
+        :return:
+        '''
+        act = QAction(name, self)
+        act.setShortcut(shortcut)
+        act.setToolTip(toolTip)
+        act.triggered.connect(callback)
+        return act
+
+
+    def createMenu(self, name):
+        '''
+
+        :param name:
+        :return:
+        '''
+        menuDic = {}
+        menuDic[name] = {}
+
+
+        return menuDic
+
+
+
+
+
---
+++ widget/sample/mayaWindow.py
@@ -0,0 +1,68 @@
+
+from import_module import *
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+import tempfile
+from widget.sample import mainWindow
+try:
+    from importlib import reload
+except:
+    pass
+
+for each in [mainWindow]:
+    reload(each)
+
+
+class mayaWindow_sample(MayaQWidgetDockableMixin, QMainWindow):
+    def __init__(self, title='SampleWindow', menubarDic={}, statusBar='', widget='',
+                 x=0, y=0, width=10, height=10):
+        super(mayaWindow_sample, self).__init__()
+
+
+        #SET WINDOW TITLE
+        self.setWindowTitle(title)
+
+        #SET WINDOW WIDGET
+        if widget != '':
+            self.setCentralWidget(widget)
+
+
+        self.setGeometry(x, y, width, height)
+
+
+        #SET STATUS BAR
+        self.statusBar().showMessage(statusBar)
+
+        #UPDATE
+        self.update()
+
+
+    def update(self):
+        '''
+
+        :return:
+        '''
+        self.setUserData()
+
+
+
+    def setUserData(self):
+        '''
+
+        :return:
+        '''
+        tempDir = tempfile.gettempdir()
+        print(tempDir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
